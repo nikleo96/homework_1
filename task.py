2630d72b341c521948f569bc27f267f4be9ff7c0
nikleo96 <71730804+nikleo96@users.noreply.github.com>
2021-09-30 20:22:16 +0300

    update task.py

--- task.py
+++ task.py
@@ -1,9 +1,40 @@
 import math
 import fractions
+import decimal
 
-a = 3
-b = -14
-c = -5
+print(decimal.Decimal("-14.0"))
+a = input("Введите коэффициент 'a': ")
+while True:
+    try:
+        a = float(a)
+        break
+    except:
+        print("Введите число!!!")
+        a = input("Введите коэффициент 'a': ")
+
+
+b = input("Введите коэффициент 'b': ")
+while True:
+    try:
+        b = float(b)
+        break
+    except:
+        print("Введите число!!!")
+        b = input("Введите коэффициент 'b': ")
+
+c = input("Введите коэффициент 'c': ")
+while True:
+    try:
+        c = float(c)
+        break
+    except:
+        print("Введите число!!!")
+        c = input("Введите коэффициент 'c': ")
+
+# a = 3
+# b = -14
+# c = -5
+
 
 D = b**2 - 4*a*c
 
@@ -15,4 +46,8 @@ elif D == 0:
 else:
     x1 = (-b + math.sqrt(D)) / (2*a)
     x2 = (-b - math.sqrt(D)) / (2*a)
-    print("Уравнение имеет два решения: x1 = ", str(x1), ", x2 = ", str(x2))
+    print("Уравнение имеет два решения: x1 = ", str(x1), ", x2 = ", str(round(x2, 2)))
+
+
+# print("Нет решения") if D < 0 else 
+
