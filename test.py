554b420739f92d9ff6de1f3034a82e6433141dc4
nikleo96 <71730804+nikleo96@users.noreply.github.com>
2021-10-05 19:24:36 +0300

    Update test.py

--- test.py
+++ test.py
@@ -9,5 +9,15 @@ print(20000*(1+0.15/12)**60)
 print(20000*(1+decimal.Decimal('0.15')/12)**60)
 print((20000*(100+decimal.Decimal(1.25))**60) / 100**60)
 
+"""
+s = 20000
+for i in range(60):
+    #s = s + round(s*(decimal.Decimal('0.15')/12), 2)
+    s = s + s*(decimal.Decimal('0.15')/12)
+    print(s)
+
+print(s)
+"""
+print(15/12)
 
 
