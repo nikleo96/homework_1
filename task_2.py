a8bb04bc6b233415e1e67a72a35e72e12337c6b0
nikleo96 <71730804+nikleo96@users.noreply.github.com>
2021-10-05 19:42:26 +0300

    add task_2.pt

---
+++ task_2.py
@@ -0,0 +1,13 @@
+input_str = input("Введите уравнение вида 'y=ax+b': ")
+
+param_x = input("Введите х: ")
+param_x = float(param_x)
+
+right_str = input_str[input_str.find("=")+1:]
+position_x = right_str.find("x")
+
+param_a = float(right_str[:position_x])
+param_b = float(right_str[position_x+1:])
+
+result = param_a * param_x + param_b
+print(result)
